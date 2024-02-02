8c1b83103dd040853e73893f9db5e168233d9b09
arifbu2105 <157726304+arifbu2105@users.noreply.github.com>
2024-02-02 09:51:04 +0700

    Add files via upload

---
+++ 2024_01_30_122627_detail_transaksi.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_detail_transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_transaksi');
+            $table->integer('id_barang')->nullable();
+            $table->integer('qty')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_detail_transaksi');
+    }
+};
